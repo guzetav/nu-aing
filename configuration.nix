be97799db75d9310a74382b50b63899ec8379397
guzetav <agelegend@yahoo.com>
2026-05-25 00:00:57 +0900

    update

--- configuration.nix
+++ configuration.nix
@@ -357,7 +357,7 @@
     gnome-boxes virt-viewer samba cifs-utils numlockx
     xorg.xrdb terminus_font pkgs.mint-themes ntfs3g
     gemini-cli zsh-completions btop ffmpegthumbnailer libnotify ulauncher
-    fzf fd plank ncdu yazi zoxide nh
+    fzf fd plank ncdu yazi zoxide nh alacritty
   ];
 
   fonts.packages = with pkgs; [
